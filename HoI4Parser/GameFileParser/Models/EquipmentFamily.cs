7c93244406910dc6c993db55aa68825ea4070891
Albert Deng <albertdeng94@outlook.com>
2021-10-11 22:01:59 -0400

    Apply archetype values to child objects

--- HoI4Parser/GameFileParser/Models/EquipmentFamily.cs
+++ HoI4Parser/GameFileParser/Models/EquipmentFamily.cs
@@ -19,7 +19,30 @@ namespace GameFileParser.Models
             EquipmentList = parser.Parse(new LandEquipmentShell()).StatisticsList;
 
             // Iterate through and fetch archetype information
+            Dictionary<string, LandEquipment> temp = new Dictionary<string, LandEquipment>();
+            for(int i = EquipmentList.Count - 1; i >= 0; i--)
+            {
+                temp.Add(EquipmentList[i].ID, EquipmentList[i]);
+            }
 
+            // Iterate over and correct information
+            for(int j = EquipmentList.Count - 1; j >= 0; j--)
+            {
+                if(!EquipmentList[j].IsArchetype)
+                {
+                    LandEquipment archetype = temp[EquipmentList[j].Archetype];
+                    EquipmentList[j].AirAttack = archetype.AirAttack;
+                    EquipmentList[j].APAttack = archetype.APAttack;
+                    EquipmentList[j].HardAttack = archetype.HardAttack;
+                    EquipmentList[j].SoftAttack = archetype.SoftAttack;
+                    EquipmentList[j].ArmorValue = archetype.ArmorValue;
+                    EquipmentList[j].Hardness = archetype.Hardness;
+                    EquipmentList[j].Breakthrough = archetype.Breakthrough;
+                    EquipmentList[j].Defense = archetype.Defense;
+                    EquipmentList[j].MaximumSpeed = archetype.MaximumSpeed;
+                    EquipmentList[j].Reliability = archetype.Reliability;
+                }
+            }
         }
     }
 }
